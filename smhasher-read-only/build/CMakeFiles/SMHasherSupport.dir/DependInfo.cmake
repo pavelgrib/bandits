2f24f5bf5b732a391d38cca5e4300ec2d82bdbbd
pavelgrib <paul.quant@gmail.com>
2014-03-15 21:29:50 -0400

    moved murmurhash function into repo

---
+++ smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/paul/github/smhasher-read-only/PMurHash.c" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/PMurHash.o"
+  )
+SET(CMAKE_C_COMPILER_ID "Clang")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/paul/github/smhasher-read-only/AvalancheTest.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/AvalancheTest.o"
+  "/Users/paul/github/smhasher-read-only/Bitslice.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Bitslice.o"
+  "/Users/paul/github/smhasher-read-only/Bitvec.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Bitvec.o"
+  "/Users/paul/github/smhasher-read-only/City.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/City.o"
+  "/Users/paul/github/smhasher-read-only/CityTest.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/CityTest.o"
+  "/Users/paul/github/smhasher-read-only/DifferentialTest.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/DifferentialTest.o"
+  "/Users/paul/github/smhasher-read-only/Hashes.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Hashes.o"
+  "/Users/paul/github/smhasher-read-only/KeysetTest.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/KeysetTest.o"
+  "/Users/paul/github/smhasher-read-only/MurmurHash1.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/MurmurHash1.o"
+  "/Users/paul/github/smhasher-read-only/MurmurHash2.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/MurmurHash2.o"
+  "/Users/paul/github/smhasher-read-only/MurmurHash3.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/MurmurHash3.o"
+  "/Users/paul/github/smhasher-read-only/Platform.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Platform.o"
+  "/Users/paul/github/smhasher-read-only/Random.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Random.o"
+  "/Users/paul/github/smhasher-read-only/SpeedTest.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/SpeedTest.o"
+  "/Users/paul/github/smhasher-read-only/Spooky.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Spooky.o"
+  "/Users/paul/github/smhasher-read-only/SpookyTest.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/SpookyTest.o"
+  "/Users/paul/github/smhasher-read-only/Stats.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Stats.o"
+  "/Users/paul/github/smhasher-read-only/SuperFastHash.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/SuperFastHash.o"
+  "/Users/paul/github/smhasher-read-only/Types.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/Types.o"
+  "/Users/paul/github/smhasher-read-only/crc.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/crc.o"
+  "/Users/paul/github/smhasher-read-only/lookup3.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/lookup3.o"
+  "/Users/paul/github/smhasher-read-only/md5.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/md5.o"
+  "/Users/paul/github/smhasher-read-only/sha1.cpp" "/Users/paul/github/smhasher-read-only/build/CMakeFiles/SMHasherSupport.dir/sha1.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
